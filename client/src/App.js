73d9b56bfe2ddddd186740aaa7179fb1d635387a
Khalilah <dygkhalilah@gmail.com>
2021-01-03 12:45:08 +0800

    test 1 client and API added

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div className="App">
+      <h1>HI KHALILAH</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ model/database.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+const con = mysql.createConnection({
+  host: DB_HOST || "127.0.0.1",
+  user: DB_USER || "root",
+  password: DB_PASS,
+  database: DB_NAME || "makanbah", // dont forget to change this
+  multipleStatements: true
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+
+  // let sql = "DROP TABLE IF exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id)); "; // change this too
+  let sql = "DROP database IF exists `makanbah`; create database makanbah; use makanbah;"
+  + "CREATE TABLE `restaurants` (`id` INT(25) NOT NULL AUTO_INCREMENT,`name` varchar(50) NOT NULL,`address` varchar(50) NOT NULL,`city` varchar(50) NOT NULL,`state` varchar(50) NOT NULL,`country` varchar(50) NOT NULL,`ratings` varchar(50) NOT NULL,`opening hours` varchar(50) NOT NULL,`api_reviews` varchar(100) NOT NULL,`type_id` INT(25) NOT NULL,PRIMARY KEY (`id`));"
+  + "CREATE TABLE `bucket_list` (`id` INT(25) NOT NULL AUTO_INCREMENT,`restaurant_id` INT(25) NOT NULL,`complete` BOOLEAN NOT NULL,`user_review` varchar(100) NOT NULL,PRIMARY KEY (`id`));"
+  + "CREATE TABLE `type` (`id` INT(25) NOT NULL AUTO_INCREMENT,`breakfast` varchar(100) NOT NULL,`lunch` varchar(100) NOT NULL,`dinner` varchar(100) NOT NULL,PRIMARY KEY (`id`));"
+  + "CREATE TABLE `user` (`id` INT(25) NOT NULL AUTO_INCREMENT,`bucket_list_id` INT(25) NOT NULL,PRIMARY KEY (`id`));"
+  + "ALTER TABLE `restaurants` ADD CONSTRAINT `restaurants_fk0` FOREIGN KEY (`type_id`) REFERENCES `type`(`id`);"
+  + "ALTER TABLE `bucket_list` ADD CONSTRAINT `bucket_list_fk0` FOREIGN KEY (`restaurant_id`) REFERENCES `restaurants`(`id`);"
+  + "ALTER TABLE `user` ADD CONSTRAINT `user_fk0` FOREIGN KEY (`bucket_list_id`) REFERENCES `bucket_list`(`id`);"
+  
+
+  
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Tables creation `makanbah` db were successful!"); // change this
+
+    console.log("Closing...");
+  });
+
+  con.end();
+});
+
+
