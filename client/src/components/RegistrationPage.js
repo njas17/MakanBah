06f0a5cc2a07ca8d93a0a331c24193dc7da68416
Jannah <jannahworks@gmail.com>
2021-01-18 15:26:14 +0800

    error handling and form validation in registration

--- client/src/components/RegistrationPage.js
+++ client/src/components/RegistrationPage.js
@@ -8,11 +8,11 @@ class RegistrationPage extends Component {
                 firstname: '',
                 lastname: '',
                 email: '',
-                password: '',                
+                password: '',
             },
             isRegistered: false,
-            isDuplicateEmail: false,
-            isLogged: false
+            isLogged: false,
+            errorMesg: ''
         }
     }
 
@@ -20,83 +20,114 @@ class RegistrationPage extends Component {
         const name = event.target.name;
         const value = event.target.value;
 
-        this.setState({ user: { ...this.state.user, [name]: value } }); 
+        this.setState({ user: { ...this.state.user, [name]: value } });
     }
 
     addUser = () => {
         fetch("/auth/users", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(this.state.user)
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.state.user)
         })
-          .then(response => {
-            response.json();
-            this.setState({ isRegistered: true });
-          })
-          .catch(error => {
-            console.error("Error in add: ", error);
-          });
+            .then(response => {
+                response.json();
+                this.setState({ isRegistered: true });
+            })
+            .catch(error => {
+                console.error("Error in add: ", error);
+            });
     };
 
     checkUserEmail = () => {
         fetch("/auth/users/" + this.state.user.email)
             .then(response => response.json())
             .then(data => {
-                this.setState({ isDuplicateEmail: data.length })
+                const val = (data.length > 0) ? true : false
+                if (val) 
+                    this.setState({ errorMesg: "Sorry. The email you have provided is already registered in this site."})
             })
             .catch(error => {
-              console.error("Error in add: ", error);
-            });        
+                console.error("Error in check email: ", error);
+                this.setState({ errorMesg: "Error on email. Please ensure email is provided."})
+            });
     };
 
     handleSubmit = (event) => {
         event.preventDefault();
+        this.setState({ errorMesg: "" });
+        const user = this.state.user;
+        if (user.firstname === "" || user.lastname === "" || user.email === "" || user.password === "") {
+            this.setState({ errorMesg: "All fields are required. Please provide the information." });
+            return;
+        }
+            
         this.checkUserEmail();
 
-        if (this.state.isDuplicateEmail === false)
+        if (this.state.errorMesg !== "")
             this.addUser();
     }
 
+    handleReset = (event) => {
+        event.preventDefault();
+        this.setState({
+            user: {
+                firstname: '',
+                lastname: '',
+                email: '',
+                password: ''
+            },
+            errorMesg: ''
+        })
+    }
+
     render() {
-        return (            
+        const { isRegistered, errorMesg, user } = this.state;
+        return (
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-8"></div>
-                    { (!this.state.isRegistered) ? ( 
-                    <div className="col-4 registrationpage">
-                        <h4>Register your information to sign up:</h4>
-                        { (this.state.isDuplicateEmail) ? (
-                        <div className="alert alert-danger" role="alert">
-                            Sorry. The email you provided is already registered.
+                    {(!isRegistered) ? (
+                        <div className="col-4 registrationpage">
+                            <h4>Register your information to sign up:</h4>
+                            { (errorMesg !== "") ? (
+                                <div className="alert alert-danger" role="alert">
+                                    { errorMesg }
+                                </div>
+                            ) : null}
+                            <form>
+                                <div className="form">
+                                    <div className="form-group">
+                                        <label className="form-label">First Name:</label>
+                                        <input type="text" name="firstname" value={user.firstname} required onChange={(e) => this.handleChange(e)} className="form-control"  ></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Last Name:</label>
+                                        <input type="text" name="lastname" value={user.lastname} required onChange={(e) => this.handleChange(e)} className="form-control" ></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Email:</label>
+                                        <input type="email" name="email" value={user.email} required onChange={(e) => this.handleChange(e)} className="form-control"  ></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Password:</label>
+                                        <input type="password" name="password" value={user.password} required onChange={(e) => this.handleChange(e)} className="form-control" ></input>
+                                    </div>
+                                    <div className="center">
+                                        <button className="btn btn-primary" onClick={(e) => this.handleSubmit(e)} type="button">Sign Up</button> {` `}
+                                        <button className="btn btn-secondary" onClick={(e) => this.handleReset(e)} type="button">Reset</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        ) : null }
-                        <form>
-                            <div className="form-group">
-                                <label className="form-label">First Name:</label><input type="text" name="firstname" required onChange={(e) => this.handleChange(e)} className="form-control"  ></input>
-                            </div>
-                            <div className="form-group">
-                                <label className="form-label">Last Name:</label><input type="text" name="lastname" required onChange={(e) => this.handleChange(e)} className="form-control" ></input>
-                            </div>
-                            <div className="form-group">
-                                <label className="form-label">Email:</label><input type="email" name="email" required onChange={(e) => this.handleChange(e)} className="form-control"  ></input>
-                            </div>
-                            <div className="form-group">
-                                <label className="form-label">Password:</label><input type="password" name="password" required onChange={(e) => this.handleChange(e)} className="form-control" ></input>
-                            </div>
-                            <div className="center">
-                                <button className="btn btn-secondary" onClick={(e) => this.handleSubmit(e)} type="button">Sign Up</button>
-                            </div>
-                        </form>
-                    </div>
                     ) : (
-                    <div className="col-4 registrationpage">
-                        <h5>Your profile have been registered. Thank You.
+                            <div className="col-4 registrationpage">
+                                <h5>Your profile have been registered. Thank You.
                             <span>Click <a href="/loginpage">here</a> to login.</span>
-                        </h5>
-                    </div>
-                    ) }
+                                </h5>
+                            </div>
+                        )}
                 </div>
             </div>
         );
